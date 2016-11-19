61fb2fcf9d6876b51860f6e227a8f7e78d2152bc
Falak Ahmed <afalak@gmail.com>
2016-11-19 09:54:52 +0000

    mine

--- controllers/default.go
+++ controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
+	c.Data["Website"] = "Falcon Next Using beego.me"
+	c.Data["Email"] = "afalak AT gmail.com"
 	c.TplName = "index.tpl"
 }
